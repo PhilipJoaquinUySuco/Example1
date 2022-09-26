fc80f82e45515cb1bb3aa41f82c054a0e2bf7e59
PhilipJoaquinUySuco <114371549+PhilipJoaquinUySuco@users.noreply.github.com>
2022-09-26 14:48:11 +0800

    Add files via upload

---
+++ ITE-300 module day 5-6 A.cs
@@ -0,0 +1,40 @@
+﻿// The class
+public class Car
+{
+    // Class members
+    public string model;        // field
+    public string color;        // field
+    public int maxSpeed;        // field
+    public int year;        // field
+
+    public void fullThrottle()   // method
+    {
+        Console.WriteLine(model + " is going as fast as it can!");
+    }
+}
+
+
+public class car
+{
+    public static void Main(string[] args)
+    {
+        Car ford = new Car();
+        ford.model = "Mustang";
+        ford.color = "red";
+        ford.year = 1969;
+
+        Car honda = new Car();
+        honda.model = "Brio";
+        honda.color = "orange";
+        honda.year = 2019;
+
+        Console.WriteLine(ford.model);
+
+        Console.WriteLine(honda.model);
+        honda.fullThrottle();
+  
+
+    Console.ReadKey();
+    }
+}
+
---
+++ ITE-300 module day 5-6 B.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Users user = new Users();
+        user.GetUserDetails();
+        Console.WriteLine("Press Enter Key to Exit..");
+        Console.ReadKey();
+    }
+}
+
+public class Users
+{
+    public string name = ("Julia");
+       public int age = 25;
+
+    public void GetUserDetails()
+    {
+        Console.WriteLine("Name: {0}, Age: {1}", name, age);
+    }
+}
